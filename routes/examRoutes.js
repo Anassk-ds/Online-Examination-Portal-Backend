bf3e3b61b6233aac1efcdbe6def0099716927279
BatchuUmaNagaSriAkhil <23ht1a4407@gmail.com>
2026-07-20 18:38:56 +0530

    Update examRoutes.js

--- routes/examRoutes.js
+++ routes/examRoutes.js
@@ -1,1 +1,12 @@
+import { Router } from 'express';
+import { getExams, getExamById, createExam, updateExam, deleteExam } from '../controllers/examController.js';
 
+const router = Router();
+
+router.get('/', getExams);
+router.get('/:id', getExamById);
+router.post('/', createExam);
+router.put('/:id', updateExam);
+router.delete('/:id', deleteExam);
+
+export default router;
