f8439676fb4644ac0629c6a2014c5410a9c77c2c
Anas Shaik <anasshaik382@gmail.com>
2026-07-24 14:11:36 +0530

    Add route to get all students

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getPendingStudents, approveStudent, getAdminStats } from '../controllers/userController.js';
+import { getPendingStudents, approveStudent, getAdminStats, getAllStudents } from '../controllers/userController.js';
 
 const router = Router();
 
 router.get('/pending', getPendingStudents);
+router.get('/students', getAllStudents);
 router.patch('/:id/approve', approveStudent);
 router.get('/stats', getAdminStats);
 
